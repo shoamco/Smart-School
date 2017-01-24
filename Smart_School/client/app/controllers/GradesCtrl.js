f7a5179c952681a5d592df5a69c8b16669708989
shoamco <shoamc98@gmail.com>
2017-01-24 11:30:12 +0200

    table geades-fix

--- Smart_School/client/app/controllers/GradesCtrl.js
+++ Smart_School/client/app/controllers/GradesCtrl.js
@@ -1,7 +1,11 @@
 
-app.controller('GradesCtrl',function($scope,$routeParams,classesService) {
+app.controller('GradesCtrl',function($scope,$routeParams,classesService,studentsService) {
     $scope.id = $routeParams.id;
+
+
+   // alert($scope.AllStudents);
     var promise = classesService.getClasses();
+    var promise2=studentsService.getStudents()
     promise.then(function (data)
     {
         $scope.Classes=data.data;
@@ -23,36 +27,61 @@ app.controller('GradesCtrl',function($scope,$routeParams,classesService) {
 
 
 
+        promise2.then(function (data)
+        {
+            $scope.AllStudents=data.data;
+
+
 
    $scope.findGread = function(studentid,courseid) {///the function get student and course and return gread of cours
-   	console.log("hello");
-   	console.log($scope.MyStudents);
-        for (var i = 0; i < $scope.MyStudents.length; i++) {
-            if ($scope.MyStudents[i].StudentId ==  studentid) {
-            	console.log($scope.MyStudents[i].StudentId);
-                for (var j = 0; j < $scope.MyStudents.Courses.length; i++) {
-                    if ($scope.MyStudents.Courses.CourseId[i] ==courseid) {
-                        return $scope.MyStudents.Courses.CourseId[i].Grade;
+
+        for (var i = 0; i < $scope.AllStudents.length; i++) {
+
+            if ($scope.AllStudents[i].StudentId ==  studentid) {
+
+                for (var j = 0; j < $scope.AllStudents[i].Courses.length; j++) {
+
+                    if ($scope.AllStudents[i].Courses[j].CourseId ==courseid) {
+
+                        return $scope.AllStudents[i].Courses[j].Grade;
+
                     }
                 }
             }
         }
+
     }
 
-    $scope.findEvaluation = function(studentid,courseid) {///the function get student and course and return Evaluation of cours
 
-            for (var i = 0; i < $scope.MyStudents.length; i++) {
-                if ($scope.MyStudents[i].StudentId ==  studentid) {
 
-                    for (var j = 0; j < $scope.MyStudents.Courses.length; i++) {
-                        if ($scope.MyStudents.Courses.CourseId[i] ==courseid) {
-                            return $scope.MyStudents.Courses.CourseId[i].Evaluation;
+            $scope.findEvaluation = function(studentid,courseid) {///the function get student and course and return Evaluation of cours
+
+
+                for (var i = 0; i < $scope.AllStudents.length; i++) {
+
+                    if ($scope.AllStudents[i].StudentId ==  studentid) {
+
+                        for (var j = 0; j < $scope.AllStudents[i].Courses.length; j++) {
+
+                            if ($scope.AllStudents[i].Courses[j].CourseId ==courseid) {
+
+                                return $scope.AllStudents[i].Courses[j].Evaluation;
+
+                            }
                         }
                     }
                 }
             }
-        }
-    /*var a=findGread(101,10);
-        alert(a);*/
+
+        });
+
+
+
+
+
+
 });
+
+
+
 });
