390a46907b26e125d9da043012bdae23bd820526
chanami <chanami15@gmail.com>
2017-02-05 17:11:22 +0200

    injection

--- Smart_School/client/app/controllers/CertificateCtrl.js
+++ Smart_School/client/app/controllers/CertificateCtrl.js
@@ -1,42 +1,24 @@
 
 
 app.controller('CertificateCtrl',function($scope,$routeParams) {
-$scope.Certificate=function () {
-
-
-
-
-
-    if (window.XMLHttpRequest)
-        var xmlhttp = new XMLHttpRequest();
-    else
-        var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-
-    var document =
+    $scope.Certificate=function () 
+    {
+        if (window.XMLHttpRequest)
+            var xmlhttp = new XMLHttpRequest();
+        else
+            var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        var document =
+            {
+            };
+        xmlhttp.onreadystatechange = function () 
         {
-        };
-
-    xmlhttp.onreadystatechange = function () {
-
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            $scope.message1 =xmlhttp.responseText;
-
-          //  alert($scope.message1);
-            $scope.$apply();
-
-
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                $scope.message1 =xmlhttp.responseText;
+                $scope.$apply();
+            }
         }
+        xmlhttp.open('POST', 'http://localhost:5000/certificate');
+        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+        xmlhttp.send(JSON.stringify(document));
     }
-
-
-    xmlhttp.open('POST', 'http://localhost:5000/certificate');
-    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=utf-8");
-    xmlhttp.send(JSON.stringify(document));
-    //  $window.reload();
-}
-
-
-
-
-
 });
