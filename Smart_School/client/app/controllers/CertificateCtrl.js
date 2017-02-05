53bc23bb4d409091971f70fcd648ab458be95696
shoamco <shoamc98@gmail.com>
2017-02-05 17:07:16 +0200

    not finshed

--- Smart_School/client/app/controllers/CertificateCtrl.js
+++ Smart_School/client/app/controllers/CertificateCtrl.js
@@ -6,6 +6,42 @@
  */
 
 app.controller('CertificateCtrl',function($scope,$routeParams) {
+
+
+    $scope.download=function () {
+var par=2;
+alert("download");
+
+        if (window.XMLHttpRequest)
+            var xmlhttp = new XMLHttpRequest();
+        else
+            var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+
+
+        xmlhttp.onreadystatechange = function () {
+
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+     //$scope.message1 =xmlhttp.response;
+               // xmlhttp.responseText;
+           //     alert("yes");
+              //  $scope.$apply();
+                xmlhttp.response.
+                xmlhttp.responseXML;
+              //  alert(xmlhttp.res);
+             //  alert(xmlhttp.response);
+
+
+            }
+        }
+
+
+        xmlhttp.open('GET', 'http://localhost:5000/admin/download');
+        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+        xmlhttp.send();
+            }
+
+
 $scope.Certificate=function () {
 
 
@@ -24,7 +60,7 @@ $scope.Certificate=function () {
     xmlhttp.onreadystatechange = function () {
 
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            $scope.message1 =xmlhttp.responseText;
+        $scope.message1 =xmlhttp.responseText;
 
           //  alert($scope.message1);
             $scope.$apply();
@@ -34,7 +70,7 @@ $scope.Certificate=function () {
     }
 
 
-    xmlhttp.open('POST', 'http://localhost:5000/certificate');
+    xmlhttp.open('POST', 'http://localhost:5000/admin/Certificate');
     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=utf-8");
     xmlhttp.send(JSON.stringify(document));
     //  $window.reload();
